2450c96ca0651fbcaa3f90528df91ea20dc52cef
Dulce-Guardado <L24170581@culiacan.tecnm.mx>
2026-02-16 10:22:54 -0700

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,27 @@
+function setup() 
+{
+  createCanvas(800, 500);
+  background(180);
+}
+
+function draw() 
+{
+  //line(30,20,185,185);
+  //line(0,250,800,250 );
+  //line(400,0,400,800 );
+  //line(0,0,1280,800 );
+  //line(0,500,800,0 );
+  //circle (400,250,50);
+  //fill(100,40,200);
+
+  stroke(0);
+  line(30,20,185,20 );
+  stroke(126);
+  noStroke();
+
+  line(30,20,185,20 );
+  line(185,20,185,175 );
+  stroke(255);
+  line(185,175,30,175 );
+  line(30,175,30,20 );
+}
